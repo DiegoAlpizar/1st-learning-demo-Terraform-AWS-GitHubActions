1b1c18c964144b2adf287eabd51c35026f08d467
Diego Alpízar <48010731+DiegoAlpizar@users.noreply.github.com>
2023-12-21 17:11:32 -0600

    Squashed '.S3-Global-Terraform-State/' content from commit 4694f1b
    git-subtree-dir: .S3-Global-Terraform-State
    git-subtree-split: 4694f1b5fa11a6d8253c37add5460b4ab4574d4e

---
+++ main.tf
@@ -0,0 +1,89 @@
+terraform {
+
+  # Partial configuration. The other settings (e.g., bucket, region) 
+  # will be passed in from a file via -backend-config arguments to 
+  # 'terraform init'
+  backend "s3" {
+    
+    key = "global/terraform.tfstate"
+    
+  }
+
+  required_providers {
+
+    aws = {
+
+      source = "hashicorp/aws"
+      version = "5.6.2"
+
+    }
+
+  }
+  
+}
+
+
+provider "aws" {
+
+  region = "us-east-1"
+  
+}
+
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = "global-terraform-state-demo"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration { status = "Enabled" }
+  
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  
+  name = "terraform_state_lock"
+
+  billing_mode = "PROVISIONED"
+  read_capacity = 1
+  write_capacity = 1
+  
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
